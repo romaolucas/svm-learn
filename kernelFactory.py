307c60148f46419166bb64c703e7dd87f3a706a1
Lucas Romão <lucas.romao.silva@gmail.com>
2017-11-02 00:29:00 -0200

    docstring

--- kernelFactory.py
+++ kernelFactory.py
@@ -1,6 +1,14 @@
 from kernels import *
 
 class KernelFactory():
+    '''
+    Basic implementation of the factory pattern
+    given a string value it will create the 
+    corresponding kernel. If the required kernel
+    takes any argument, it's assumed to be passed
+    to the method create_kernel, otherwise it will
+    return an exception
+    '''
 
     @staticmethod
     def create_kernel(kernel_name, *args, **kwargs):
