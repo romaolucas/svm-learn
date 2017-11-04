c96aad81800d583ce98ef0a5a38056c3e92d9142
Lucas Romão <lucas.romao.silva@gmail.com>
2017-11-04 16:24:49 -0200

    finishing svm implementation

--- svm.py
+++ svm.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sklearn.datasets as datasets
+import sklearn.model_selection as model_selection
 from cvxopt import solvers, matrix
 from kernelFactory import KernelFactory
 
@@ -53,16 +55,107 @@ class SVMLearn(object):
         Computes the gram matrix. The matrix is 
         such that K(x, y) = kernel(x, y)
         '''
-        return np.array([[self._kernel(x, y) for x in self._X] for y in self._X])
+        return np.array([[self._kernel.apply(x, y) for x in self._X] for y in self._X])
+
+    def support_vectors(self):
+        '''
+        returns a set of indexes for the support vectors, that is
+        the multipliers non-zero value
+        '''
+        S = []
+        for index, multiplier in enumerate(self._multipliers):
+            if multiplier > 0:
+                S.append(index)
+        return S
+
+    def multipliers_box_constraint(self):
+        '''
+        returns a set of indexes of the multipliers that satisfy
+        0 < multiplier < C
+        '''
+        M = []
+        for index, multiplier in enumerate(self._multipliers):
+            if multiplier > 0 and multiplier < self._C:
+                M.append(index)
+        return M
+
+    def calculate_b(self):
+        '''
+        Calculates the b, such that y = w'x + b
+        b = 1/N^{M} \sum_{n \in M} (t_n - \sum_{m \in S} multiplier_m*t_m*k(x_n, x_m)
+        S = set of support vectors, i.e. multiplier > 0
+        M = set of multipliers with 0 < multiplier < C
+        '''
+        M = self.multipliers_box_constraint()
+        S = self.support_vectors()
+        K = self.gram_matrix()
+        b = 0
+        for n in M:
+            partial_sum = 0
+            for m in S:
+                partial_sum += self._multipliers[m]*self._t[m]*K[n][m]
+            b += self._t[n] - partial_sum
+        b = b/len(M)
+        return b
 
     def fit(self):
+        '''
+        Computes the lagrange multipliers solving the quadric problem
+        G is the matrix representing the box constraints
+        A is the matrix representing \sum_{n} multipliers_n*t_n = 0
+        P is the matrix with entrances t_i*t_j*k(x_i, x_j)
+        '''
         K = self.gram_matrix()
-        T = np.outer(t, t)
-        P = matrix(np.dot(-1*K, T))
+        T = np.outer(self._t, self._t)
+        P = matrix(T * K)
         q = matrix(np.ones(self._N))
-        G_C = np.eye(self._N)
-        G_zero = np.diag((-1)*np.ones(self._N))
-        h_C = np.ones(self._N) * self._C
-        h_zero = np
-        G = matrix(np.vstack((G_zero, G_C)))
+        G_C = matrix(np.eye(self._N))
+        G_zero = matrix(np.diag((-1)*np.ones(self._N)))
+        h_C = matrix(np.ones(self._N) * self._C)
+        h_zero = matrix(np.zeros(self._N))
+        G = matrix(np.vstack((G_C, G_zero)))
         h = matrix(np.vstack((h_C, h_zero)))
+        A = matrix(self._t.astype(np.double), (1, self._N))
+        b = matrix(0.0)
+        solution = solvers.qp(P, q, G, h, A, b)
+        self._multipliers = np.ravel(solution['x'])
+        print("multipliers ", self._multipliers)
+        self._b = self.calculate_b()
+
+    def train(self, X):
+        '''
+        classify a set of inputs in X using the formula
+        y(x) = b + \sum_{i \in S} t[i]*multipliers[i]*k(x, x[i])
+        with S being the set of the support vectors indexes
+        the label for x is the signal of y(x)
+        '''
+        S = self.support_vectors()
+        y = []
+        for x in X:
+            y_x = self._b
+            for m in S:
+                y_x += self._multipliers[m]*self._t[m]*self._kernel.apply(x, self._X[m])
+            y.append(1 if y_x >= 0 else -1)
+        return y
+
+    def compute_accuracy(self, X, t):
+        y = self.train(X)
+        correctly_classified = 0
+        for y_i, t_i in zip(y, t):
+            if y_i == t_i:
+                correctly_classified += 1
+        return correctly_classified / len(t)
+
+def main():
+    X, t = datasets.load_breast_cancer(return_X_y=True)
+    for index, lbl in enumerate(t):
+        if lbl == 0:
+            t[index] = -1
+    X_train, X_test, t_train, t_test = model_selection.train_test_split(X, t)
+    svm = SVMLearn(X_train, t_train)
+    svm.fit()
+    print("finished training")
+    print("Accuracy: ", svm.compute_accuracy(X_test, t_test))
+
+if __name__ == '__main__':
+    main()
